b896722edafe7f0a18d89da6e9ec9be023ba6aef
sergio-coti <sergio.coti@gmail.com>
2023-04-26 11:49:16 -0300

    first commit

---
+++ ApiPedidos.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using ApiPedidos.Domain.Entities;
+using ApiPedidos.Infra.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPedidos.Infra.Data.Contexts
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "BDApiPedidos");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+            modelBuilder.ApplyConfiguration(new EnderecoConfiguration());
+            modelBuilder.ApplyConfiguration(new CobrancaConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemPedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+        }
+
+        public DbSet<Cliente>? Clientes { get; set; }
+        public DbSet<Endereco>? Enderecos { get; set; }
+        public DbSet<Cobranca>? Cobrancas { get; set; }
+        public DbSet<ItemPedido>? ItensPedido { get; set; }
+        public DbSet<Pedido>? Pedidos { get; set; }
+    }
+}
---
+++ ApiPedidos.Services/Program.cs
@@ -0,0 +1,35 @@
+using ApiPedidos.Application.Interfaces;
+using ApiPedidos.Application.Services;
+using ApiPedidos.Infra.EventBus.Producers;
+using ApiPedidos.Infra.EventBus.Settings;
+using ApiPedidos.Services.Configurations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+DependencyInjectionConfiguration.AddDependencyInjection(builder);
+CorsConfiguration.AddCors(builder);
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+CorsConfiguration.UseCors(app);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { }
